b162e7daa1ad598c2debe6c21673c23995f93fb5
nan_lin <nan_lin@zixun.com>
2022-03-15 21:33:05 +0800

    feat: adb启动器完善流程和展示

--- src-py/adb/bootstrap/__init__.py
+++ src-py/adb/bootstrap/__init__.py
@@ -6,6 +6,10 @@ class Bin:
     @staticmethod
     def bootstrap_adb() -> bool:
         print('Bin bootstrap')
+        subprocess.Popen('adb.exe kill-server',
+                         shell=True,
+                         cwd=bin.adb_path).communicate()
+
         subprocess.Popen('adb.exe start-server',
                          shell=True,
                          cwd=bin.adb_path).communicate()
@@ -16,5 +20,10 @@ class Bin:
                                      cwd=bin.adb_path,
                                      stdout=subprocess.PIPE,
                                      stderr=subprocess.PIPE).communicate()
+
+        print('Bin current connected devices output:')
+
+        for line in output.splitlines():
+            print(line)
 
         return len(output.splitlines()) > 2

1fd802887cdb956f90a62f8789a00188d822964b
forforforStudy <411162428@qq.com>
2022-03-22 00:19:31 +0800

    feat: 完善adb进行app启动调用

--- src-py/adb/__init__.py
+++ src-py/adb/__init__.py
@@ -5,6 +5,7 @@ import bootstrap
 from adb.bootstrap import ADBDevice, device
 from adb.command.list_package import list_package
 from adb.command.screencap import screencap, ScreencapResult
+from adb.command.launch_app import launch_app
 from adb.invoker import run
 
 
@@ -42,8 +43,8 @@ class ADB:
         :return:
         """
 
-        for device in self.current_devices:
-            self.screencaps.insert(0, screencap(device))
+        for current_device in self.current_devices:
+            self.screencaps.insert(0, screencap(current_device))
 
         return self
 
@@ -53,7 +54,11 @@ class ADB:
         :return:
         """
 
-        return [(device, list_package(device)) for device in self.current_devices]
+        return [(current_device, list_package(current_device)) for current_device in self.current_devices]
+
+    def launch_app(self, app_keyword: str):
+        for current_device in self.current_devices:
+            launch_app(app_keyword, current_device)
 
     def latest_screencap(self):
         return self.screencaps[0]
@@ -62,8 +67,4 @@ class ADB:
 if __name__ == '__main__':
     adb_ins = ADB.ready().use_first_device()
 
-    for list_result in adb_ins.get_package_of_list():
-        (device, package_list) = list_result
-        print('device_id: {}'.format(device.device_id))
-        for package_name in package_list:
-            print('\t{}'.format(package_name))
+    adb_ins.launch_app('azurlane')
--- src-py/adb/command/launch_app.py
+++ src-py/adb/command/launch_app.py
@@ -1,12 +1,31 @@
+import re
+
 from adb.command.list_package import list_package
+from adb.invoker import run
 from adb.bootstrap import ADBDevice
 
 
-def launch_app(keyword: str, device: ADBDevice = None) -> str:
+def launch_app(keyword: str, device: ADBDevice = None):
     list_results = list_package(device)
     target_package_name = next(package_name for package_name in list_results if keyword in package_name)
 
     if target_package_name is None:
         raise RuntimeError('未找到关键词为: {} 的app包'.format(keyword))
     else:
-        return target_package_name
+        target_package_name = target_package_name.replace('package:', '')
+
+        print('launch_app - 查找到关键词: "{}" 包名: "{}"'.format(keyword, target_package_name))
+        print('launch_app - 准备获取包: "{}"的MainActively'.format(target_package_name))
+
+        output, _ = run('shell dumpsys package {}'.format(target_package_name))
+        for output_line in bytes.decode(output).splitlines():
+            match_obj = re.search('{}\\S+'.format(target_package_name), output_line)
+            if match_obj is not None:
+                # 开始调用运行
+                group = match_obj.group()
+
+                run('shell am start {}'.format(group))
+                print('launch_app - 成功运行App: {}'.format(group))
+                return
+
+    raise RuntimeError('未能成功执行关键词为: "{}" 的任何App'.format(keyword))

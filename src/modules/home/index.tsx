e87cc7b2e4c90a9c41589db08492c2445e27d3e2
nan_lin <nan_lin@zixun.com>
2022-04-01 11:18:04 +0800

    feat: 添加react-router-dom能力分路由

---
+++ src/modules/home/index.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Switch, Link, Route } from 'react-router-dom'
+
+import App from '../app/App'
+import Screencaps from '../screencaps'
+
+export default function Home() {
+  return (
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">App</Link>
+        </li>
+        <li>
+          <Link to="/screencaps">Screencaps</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path='/'>
+          <App />
+        </Route>
+        <Route exact path='/screencaps'>
+          <Screencaps />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
---
+++ src/modules/screencaps/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Screencaps() {
+  return (
+    <div>
+      Screencaps
+    </div>
+  )
+}
+
+export default Screencaps

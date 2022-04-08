87ab951d47a496110c05a611040a2572286fd782
nan_lin <411162428@qq.com>
2022-04-08 15:15:03 +0800

    fix: 修复截图多线程无法暂停问题

--- src-py/resources/screencap_thread.py
+++ src-py/resources/screencap_thread.py
@@ -1,26 +1,33 @@
 import threading
 import time
 
+from adb import ADB
+
 stop_screencap = True
+
+
+def running_target():
+    global stop_screencap
+
+    while True and stop_screencap is False:
+        adb_ins = ADB.ready().use_first_device()
+        adb_ins.screencap_once()
+        time.sleep(2)
 
 
 def stop_screencap_threads():
     global stop_screencap
-
-    stop_screencap = False
+    stop_screencap = True
 
 
 def start_screencap_thread():
-    def running_target():
-        from adb import ADB
-        global stop_screencap
-
-        while True and stop_screencap is False:
-            adb_ins = ADB.ready().use_first_device()
-            adb_ins.screencap_once()
-            time.sleep(2)
-
     global stop_screencap
     stop_screencap = False
     current_thread = threading.Thread(target=running_target)
     current_thread.start()
+
+
+def is_screencaps_thread_doing() -> bool:
+    global stop_screencap
+
+    return stop_screencap
